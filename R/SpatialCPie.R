016a623cc501a4c4e461b6dd5549ffd8e7e96b20
Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>
2018-09-04 16:55:06 +0200

    Fix indent

--- R/SpatialCPie.R
+++ R/SpatialCPie.R
@@ -344,12 +344,12 @@ globalVariables(c(
         scale_size(guide = "none", range = c(2, 7)) +
         theme_bw() +
         theme(
-              axis.ticks.x = element_blank(),
-              axis.ticks.y = element_blank(),
-              axis.title.x = element_blank(),
-              axis.title.y = element_blank(),
-              axis.text.x = element_blank(),
-              axis.text.y = element_blank()
+            axis.ticks.x = element_blank(),
+            axis.ticks.y = element_blank(),
+            axis.title.x = element_blank(),
+            axis.title.y = element_blank(),
+            axis.text.x = element_blank(),
+            axis.text.y = element_blank()
         )
 }
 
