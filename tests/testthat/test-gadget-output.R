6ab087cc3adbfd834818558ff7a2c7a05bce4205
Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>
2018-09-23 11:22:44 +0200

    Skip output test on bioc build server

--- tests/testthat/test-gadget-output.R
+++ tests/testthat/test-gadget-output.R
@@ -3,5 +3,6 @@ context("Gadget output")
 library(shinytest)
 
 test_that("Gadget output hasn't changed", {
+    skip_on_bioc()
     expect_pass(testApp("./cases/default/", compareImages = FALSE))
 })
